85131fc6fafeb4223eed88ae17812502e05e1b5b
agency1412-oss <agency1412@gmail.com>
2025-10-20 15:30:19 +0700

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          50: '#e6f2f7',
+          100: '#cce5ef',
+          200: '#99cbdf',
+          300: '#66b1cf',
+          400: '#3397bf',
+          500: '#006699',
+          600: '#00527a',
+          700: '#003d5c',
+          800: '#00293d',
+          900: '#00141f',
+        },
+      },
+    },
   },
   plugins: [],
 };
